e5605afd54b9d6e453e1a2b25d589d2a7dc26975
xilo <ambrosdavid99@gmail.com>
2020-05-12 23:53:42 +0200

    Added crazy bots

---
+++ BotCurl/botmain_flo.js
@@ -0,0 +1,144 @@
+var request = require('request');
+
+const ELAPSED_TOKEN_TIME = 1000 * 60 * 20; // every 20 minutes (in ms 1000*60*20) 
+
+function first_boot(){
+    var startDate = new Date();
+
+    const SEC_DELAY = 60 * 1 * 1000;
+    const USER_NAME = "marco4949_lol";
+    const PASS_ENCRYPTED = "%23PWD_INSTAGRAM_BROWSER%3A10%3A1589313412%3AAahQAGb3EaEDEQXKWug8MRDDElSU1SVR%2Boz185A07RGeNhQBilpoh4pCo8FuPyPvc1LHkC%2F%2BasGdqXB%2Bzx67khd2bhieOl8Q%2FIHervW5DdTKn2DG%2BDlc92EtdHR0rz1QmYW1el%2BmaCNlsUtGn7m9dCM%3D&queryParams=%7B%7D&optIntoOneTap=false"
+    const POST_ID = "2307410364945516138"; // FEDEROSSI
+
+    var headers = {
+        'authority': 'www.instagram.com',
+        'x-ig-www-claim': 'hmac.AR1qoTFzoWymVJcalZt1F1YNOYu67iKVp3NRMm9q_vbBKGZR',
+        'x-instagram-ajax': '9b09d2a60ea6',
+        'content-type': 'application/x-www-form-urlencoded',
+        'accept': '*/*',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/81.0.4044.122 Chrome/81.0.4044.122 Safari/537.36',
+        'x-requested-with': 'XMLHttpRequest',
+        'x-csrftoken': 'W914EH38Ir9sHzRAPEwrNScC5u63UPLw',
+        'x-ig-app-id': '936619743392459',
+        'origin': 'https://www.instagram.com',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.instagram.com/',
+        'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,ca;q=0.6',
+        'cookie': 'ig_cb=1; ig_did=FD1CB2FC-A47B-40AB-A022-E22EC4241824; mid=XrrzYQAEAAHpTRO-mkMjasUdSkLh; shbid=15129; shbts=1589310344.641645; rur=FTW; csrftoken=W914EH38Ir9sHzRAPEwrNScC5u63UPLw; urlgen="{\"5.170.200.75\": 16232}:1jYb1C:rRIpmgSAwOylJTwOtvD6enTqVN8"'
+    };
+
+    var dataString = 'username=' + USER_NAME + '&enc_password=' + PASS_ENCRYPTED;
+
+    var options = {
+        url: 'https://www.instagram.com/accounts/login/ajax/',
+        method: 'POST',
+        headers: headers,
+        body: dataString
+    };
+
+
+    function login(error, response, body) {
+
+        let csrftoken ="",sessionid="",urlgen="",ds_user_id="";
+        let cookies;
+        if (!error && response.statusCode == 200) {
+            cookies = response.headers["set-cookie"];
+
+            cookies.forEach(cookie => {
+                if(cookie.startsWith("csrftoken")){
+                    csrftoken = cookie.substring(cookie.indexOf("=")+1, cookie.indexOf(";"));
+                }
+                if(cookie.startsWith("urlgen")){
+                    urlgen = cookie.substring(cookie.indexOf("=")+1, cookie.indexOf(";"));
+                }            
+                if(cookie.startsWith("sessionid")){
+                    sessionid = cookie.substring(cookie.indexOf("=")+1, cookie.indexOf(";"));
+                }
+                if(cookie.startsWith("ds_user_id")){
+                    ds_user_id = cookie.substring(cookie.indexOf("=")+1, cookie.indexOf(";"));
+                }
+            });
+
+            //console.log(response.headers);
+            console.log("csfrffr : " + csrftoken );
+            console.log("sessionid : " + sessionid );
+            console.log("urlgen : " + urlgen );
+            console.log("finito?");
+
+            comment(csrftoken,sessionid,urlgen,ds_user_id);
+        }
+    }
+
+    request(options, login);
+
+    function comment(csrftoken,sessionid,urlgen,ds_user_id){
+        console.log("ok ora commento");
+
+        var headersCommento = {
+            'authority': 'www.instagram.com',
+            'x-ig-www-claim': 'hmac.AR1qoTFzoWymVJcalZt1F1YNOYu67iKVp3NRMm9q_vbBKGZR',
+            'x-instagram-ajax': '9b09d2a60ea6',
+            'content-type': 'application/x-www-form-urlencoded',
+            'accept': '*/*',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/81.0.4044.122 Chrome/81.0.4044.122 Safari/537.36',
+            'x-requested-with': 'XMLHttpRequest',
+            'x-csrftoken': csrftoken,
+            'x-ig-app-id': '936619743392459',
+            'origin': 'https://www.instagram.com',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': 'https://www.instagram.com/p/CAFkYBxocZq/',
+            'accept-language': 'it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7,ca;q=0.6',
+            'cookie': 'ig_cb=1; ig_did=FD1CB2FC-A47B-40AB-A022-E22EC4241824; mid=XrrzYQAEAAHpTRO-mkMjasUdSkLh; shbid=15129; shbts=1589310344.641645; rur=FTW; csrftoken='+csrftoken+'; ds_user_id=' + ds_user_id + ' ; sessionid=' + sessionid + '; urlgen=' + urlgen + ""
+        };
+        
+        var dataStringCommento = 'comment_text=1&replied_to_comment_id=';
+        
+        var optionsCommento = {
+            url: 'https://www.instagram.com/web/comments/'+POST_ID+'/add/',
+            method: 'POST',
+            headers: headersCommento,
+            body: dataStringCommento
+        };
+
+        let cnt = 0;
+        
+        function commento(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+                let parsedBody = JSON.parse(body);
+
+                if(parsedBody.status==="ok"){
+                    console.log("[msg] " + cnt + " inviato");
+                }else{
+                    console.error("[msg] " + cnt + " NON inviato");
+                }
+                
+                cnt++;
+            }else{
+                console.log("errore");
+                console.error(error);
+                console.error(response);
+                console.error(body);
+            }
+        }
+
+        function loopcomments(){
+            request(optionsCommento, commento);
+
+            var endDate = new Date() - startDate;
+            if(endDate>ELAPSED_TOKEN_TIME){
+                first_boot();
+            } else {
+                setTimeout(loopcomments,SEC_DELAY + Math.random() * 10);
+            }
+        }
+
+        loopcomments();
+    }
+}
+
+first_boot();
---
+++ BotPuppeeter/spammer.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  // Starting browser
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+
+  // Login flow
+  await page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher');
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', 'rachele_miotto00');
+  await page.type('input[name="password"]', 'salsiccia2000');
+  await page.click('button[type="submit"]');
+
+  // Waiting for page to refresh
+  await page.waitForNavigation();
+  await page.goto('https://www.instagram.com/p/CAFkYBxocZq/');
+
+  // Navigate to post and submitting the comment
+    for ( var i = 0; i <60; i++){
+    await page.goto('https://www.instagram.com/p/CAFkYBxocZq/');
+    await page.waitForSelector('textarea');
+    await page.type('textarea', '1');
+    await page.click('button[type="submit"]');
+    await new Promise(r => setTimeout(r, 40000+(Math.floor(Math.random()*10))));
+    await page.goto('https://www.instagram.com/p/CAFkYBxocZq/');
+    console.log("i....."+i+"\n");    
+        }
+    await browser.close();
+})();
+
